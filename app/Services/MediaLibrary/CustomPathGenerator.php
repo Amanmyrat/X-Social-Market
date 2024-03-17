889da3610200f73aae900e2252af3ab491e0da25
Amanmyrat <tekemuradov@gmail.com>
2024-03-17 22:58:39 +0500

    Add custom path generator for media library

---
+++ app/Services/MediaLibrary/CustomPathGenerator.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Services\MediaLibrary;
+
+use Spatie\MediaLibrary\Support\PathGenerator\PathGenerator;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Illuminate\Support\Str;
+
+class CustomPathGenerator implements PathGenerator
+{
+    public function getPath(Media $media): string
+    {
+        return $this->baseDir($media) . '/';
+    }
+
+    public function getPathForConversions(Media $media): string
+    {
+        return $this->baseDir($media) . '/conversions/';
+    }
+
+    public function getPathForResponsiveImages(Media $media): string
+    {
+        return $this->baseDir($media) . '/responsive-images/';
+    }
+
+    protected function baseDir(Media $media): string
+    {
+        $year = $media->created_at->format('Y');
+        $month = $media->created_at->format('m');
+        $hashedId = Str::limit(md5($media->model_id), 12, '');
+
+        return "$year/$month/$hashedId";
+    }
+}

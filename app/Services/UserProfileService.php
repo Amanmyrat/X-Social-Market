4ca491aee2602919d5979017b3714ab519d3606d
Amanmyrat <tekemuradov@gmail.com>
2024-04-01 18:53:54 +0500

    Fix user profile update

--- app/Services/UserProfileService.php
+++ app/Services/UserProfileService.php
@@ -12,21 +12,35 @@ class UserProfileService
     /**
      * @throws Throwable
      */
-    public function update(array $validated, User $user): void
+    public function update(User $user, array $validated): void
     {
         $user->update($validated);
-        DB::transaction(function () use ($validated, $user) {
+        if (isset($validated['profile'])) {
+            DB::transaction(function () use ($validated, $user) {
 
-            if ($user->profile) {
-                $user->profile()->update($validated['profile']);
-            } else {
-                UserProfile::create(array_merge($validated['profile'], ['user_id' => $user->id]));
-            }
+                if (isset($validated['profile']['profile_image'])) {
+                    $profileImage = $validated['profile']['profile_image'];
+                }
+                unset($validated['profile']['profile_image']);
 
-            if (isset($validated['profile']['profile_image'])) {
-                $user->profile->clearMediaCollection('user_images');
-                $user->profile->addMedia($validated['profile']['profile_image'])->toMediaCollection('user_images');
-            }
-        });
+                if ($user->profile) {
+                    $user->profile()->update($validated['profile']);
+                } else {
+                    $validated['profile'] = count($validated['profile']) > 0 ? $validated['profile'] : ['bio' => ''];
+
+                    UserProfile::create(array_merge($validated['profile'], ['user_id' => $user->id]));
+                }
+
+                $user->load('profile');
+
+                if (isset($profileImage)) {
+                    if ($user->profile->hasMedia()) {
+                        $user->profile->clearMediaCollection('user_images');
+                    }
+                    $user->profile->addMedia($profileImage)->toMediaCollection('user_images');
+                }
+            });
+        }
+
     }
 }
